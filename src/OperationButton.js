8e9151c5ea8f821815831516e2348c26e0343501
Barbara <barbara@Barbaras-MacBook-Pro.local>
2023-08-28 20:42:47 +0100

    working calculations

--- src/OperationButton.js
+++ src/OperationButton.js
@@ -2,6 +2,6 @@ import { ACTIONS } from "./App"
 
 export default function OperationButton({dispatch, operation}){
     return (
-    <button onClick={()=>operation({type: ACTIONS.CHOOSE_OPERATION, payload:{operation}})}>{operation}</button>
+    <button onClick={()=>dispatch({type: ACTIONS.CHOOSE_OPERATION, payload:{operation}})}>{operation}</button>
     )
 }
